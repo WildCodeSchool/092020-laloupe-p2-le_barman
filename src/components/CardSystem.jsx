5546059a48efa1dcb32d402580f273fef4b14052
Maxence-P <maxencepointeau@gmail.com>
2020-10-29 16:09:14 +0100

    prep branch

---
+++ src/components/CardSystem.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+class CardSystem extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name = "strDrink",
+            instructions = "strInstrutions",
+            ingredients = "On verra",
+            image = "strDrinkThumb"
+        }
+    }
+  render(){
+    return(
+      <div></div>
+    )
+  } 
+}
+
+export default CardSystem;
