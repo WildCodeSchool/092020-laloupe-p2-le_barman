1eafdca1c766e329c4274e67211d4f72448d5cde
Maxence-P <maxencepointeau@gmail.com>
2020-11-04 10:32:24 +0100

    started building the system

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import CardSystem from './components/CardSystem';
 
 function App() {
   return (
     <main className="container">
-      <Switch>
+      <CardSystem />
+      {/* <Switch>
         <Route exact path="/" component={CardSystem}/>
         <Route path="/CGU" component={CGU}/>
         <Route path="/ContactUs" component={ContactUs}/>
-      </Switch>
+      </Switch> */}
     </main>
   );
 }
--- src/components/CardSystem.jsx
+++ src/components/CardSystem.jsx
@@ -1,18 +1,50 @@
 import React from "react";
+// import axios from "axios";
 
 class CardSystem extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            name = "strDrink",
-            instructions = "strInstrutions",
-            ingredients = "On verra",
-            image = "strDrinkThumb"
+            researchName : "",
+            researchIngredient : "",
+            name : "strDrink",
+            instructions : "strInstrutions",
+            ingredients : "demandé jérémy pour filter",
+            image : "strDrinkThumb"
         }
+
     }
+
+    searchName = () => {
+      this.setState({researchName: document.getElementById('searchName').value});
+    }
+
+    searchIngredient = () => {
+      this.setState({researchIngredient: document.getElementById('searchIngredient').value});
+    }
+
+    fetchDrinksByName = () => {
+      const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${this.state.researchName}`
+      console.log(url);
+      document.getElementById('searchName').value = null;
+    }
+
+    fetchDrinksByIngredient = () => {
+      const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${this.state.researchIngredient}`
+      console.log(url);
+      document.getElementById('searchName').value = null;
+    }
+
   render(){
     return(
-      <div></div>
+      <section>
+        <div>
+          <input id="searchName" type="text" placeholder="Enter a name:" onChange={this.searchName}></input>
+          <button onClick={this.fetchDrinksByName}>Search</button>
+        </div>
+        <div>        
+        </div>
+      </section>
     )
   } 
 }
