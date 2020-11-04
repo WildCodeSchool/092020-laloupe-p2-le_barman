076e601793bffeddea8f6f27fd7b00c1353fc87f
Maxence-P <maxencepointeau@gmail.com>
2020-11-04 11:15:53 +0100

    changed css & html icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <main className="container">
-      <Header />
       <Switch>
         <Route exact path="/" component={CardSystem}/>
         <Route path="/CGU" component={CGU}/>
