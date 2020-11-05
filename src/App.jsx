82940516fee3bc605f7e33b7c53af06abc34fa0e
Sovakiiir <valentin.broudinpro@gmail.com>
2020-11-05 10:15:54 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import CGU from "./components/CguPage";
 // import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <main className="container">
       <Header />
+      <CGU />
       {/* <Switch>
         <Route exact path="/" component={CardSystem} />
         <Route path="/CGU" component={CGU} />
