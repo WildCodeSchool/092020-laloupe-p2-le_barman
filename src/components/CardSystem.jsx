e356782e44acf5be383efc4e8d24e58e1b2e7eef
Maxence-P <maxencepointeau@gmail.com>
2020-11-10 14:04:21 +0100

    changed how the card was made

--- src/components/CardSystem.jsx
+++ src/components/CardSystem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Card from "./Card";
 import './CardSystem.css';
 
 class CardSystem extends React.Component {
@@ -7,13 +8,14 @@ class CardSystem extends React.Component {
         super(props);
         this.state = {
             research : "",
-            resultDrinks : []
+            resultDrinks : [],
+            drinkIngredients : []
         }
         this.fetchDrinksbyName = this.fetchDrinksByName.bind(this);
     }
 
     getAllIngre = (array) => {
-      const newAray = [];
+      let newAray = [];
       for (let i = 1; i < 16; i += 1) {
         if (array[`strIngredient${i}`] === null) {
           return newAray;
@@ -23,12 +25,12 @@ class CardSystem extends React.Component {
       return newAray;
     }
 
-    searchName = () => {
-      this.setState({research: document.getElementById('searchName').value});
+    searchName = (event) => {
+      this.setState({research: event.target.value});
     }
 
     fetchDrinksByName = () => {
-      if (document.getElementById('searchName').value !== "") {
+      if (this.state.research !== "") {
         const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${this.state.research}`
         axios
           .get(url)
@@ -36,14 +38,13 @@ class CardSystem extends React.Component {
             return response.data;
           })
           .then((quote) => {
-            this.setState({ resultDrinks: quote.drinks });
+            this.setState({ drinkIngredients: [], });
+            this.setState({ resultDrinks: quote.drinks, research: "" });
           });
-        document.getElementById('searchName').value = null;
-      } else if (document.getElementById('searchName').value === "") {
       }
     }
 
-    fetchDrinksRandom = () => {
+    fetchDrinksRandom = () => {      
       const url = `https://www.thecocktaildb.com/api/json/v1/1/random.php`
       axios
           .get(url)
@@ -51,118 +52,9 @@ class CardSystem extends React.Component {
             return response.data;
           })
           .then((quote) => {
-            this.setState({ resultDrinks: quote.drinks[0] });
+            this.setState({ drinkIngredients: [], });
+            this.setState({ resultDrinks: [quote.drinks[0]], drinkIngredients: this.getAllIngre(quote.drinks[0]) });
           });
-    }
-
-    createCardbyName = () => {
-      // reinitialisation of the container
-      document.getElementById('cardCont').innerHTML = "";
-      for (let i = 0; i < this.state.resultDrinks.length; i++) {
-      // creation of the main frame of the card
-      const cardBody = document.createElement("div");
-      cardBody.classList.add("cardBody");
-      // creation of the subframe for the upper part
-      const cardTitle = document.createElement("div");
-      cardBody.appendChild(cardTitle);
-      cardTitle.classList.add("cardTitle");
-      // creation of the image frame
-      const cardImg = document.createElement("img");
-      cardImg.src = `${this.state.resultDrinks[i].strDrinkThumb}`;
-      cardImg.classList.add("cardImg");
-      cardTitle.appendChild(cardImg);
-      // creation of the name/ingredient frame
-      const cardNaming = document.createElement("div");
-      cardNaming.classList.add("cardNaming");
-      cardTitle.appendChild(cardNaming);
-      // creation of the name frame
-      const cardName = document.createElement("p");
-      cardName.innerText = `${this.state.resultDrinks[i].strDrink}`;
-      cardName.classList.add("cardName");
-      cardNaming.appendChild(cardName);
-      // creation of the ingredient frame
-      const cardIngd = document.createElement("ul");
-      cardIngd.classList.add("cardIngd");
-      cardNaming.appendChild(cardIngd);
-      // test ingredient making
-      const allIngredients = this.getAllIngre(this.state.resultDrinks[i]);
-      const createCardIngredient = (el) => {
-        const cardIngredient = document.createElement("li");
-        cardIngredient.innerText = `${el}`;
-        cardIngredient.classList.add("cardIngredient");
-        cardIngd.appendChild(cardIngredient);
-      }
-      allIngredients.forEach(el => createCardIngredient(el));  
-      // creation of the bottom subframe
-      const  cardBtm = document.createElement("div");
-      cardBtm.classList.add("cardBtm");
-      cardBody.appendChild(cardBtm);
-      // creation of cardWay frame
-      const cardWay = document.createElement("h4");
-      cardWay.innerText = "How to :"
-      cardBtm.appendChild(cardWay);
-      // creation of instructions frame
-      const cardInstruct = document.createElement("p");
-      cardInstruct.innerText = `${this.state.resultDrinks[i].strInstructions}`;
-      cardInstruct.classList.add("cardInstruct");
-      cardBtm.appendChild(cardInstruct);
-      //append the whole card to the container
-      document.getElementById('cardCont').appendChild(cardBody);
-    }
-    }
-
-    createCardRandom = () => {
-      // reinitialisation of the container
-      document.getElementById('cardCont').innerHTML = "";
-      // creation of the main frame of the card
-      const cardBody = document.createElement("div");
-      cardBody.classList.add("cardBody");
-      // creation of the subframe for the upper part
-      const cardTitle = document.createElement("div");
-      cardBody.appendChild(cardTitle);
-      cardTitle.classList.add("cardTitle");
-      // creation of the image frame
-      const cardImg = document.createElement("img");
-      cardImg.src = `${this.state.resultDrinks.strDrinkThumb}`;
-      cardImg.classList.add("cardImg");
-      cardTitle.appendChild(cardImg);
-      // creation of the name/ingredient frame
-      const cardNaming = document.createElement("div");
-      cardNaming.classList.add("cardNaming");
-      cardTitle.appendChild(cardNaming);
-      // creation of the name frame
-      const cardName = document.createElement("h4");
-      cardName.innerText = `${this.state.resultDrinks.strDrink}`;
-      cardName.classList.add("cardName");
-      cardNaming.appendChild(cardName);
-      // creation of the ingredient frame
-      const cardIngd = document.createElement("ul");
-      cardIngd.classList.add("cardIngd");
-      cardNaming.appendChild(cardIngd);
-      // ingredient making
-      const allIngredients = this.getAllIngre(this.state.resultDrinks);
-      const createCardIngredient = (el) => {
-        const cardIngredient = document.createElement("li");
-        cardIngredient.innerText = `${el}`;
-        cardIngredient.classList.add("cardIngredient");
-        cardIngd.appendChild(cardIngredient);
-      }
-      allIngredients.forEach(el => createCardIngredient(el));  
-      // creation of the bottom subframe
-      const  cardBtm = document.createElement("div");
-      cardBtm.classList.add("cardBtm");
-      cardBody.appendChild(cardBtm);
-      // creation of cardWay frame
-      const cardWay = document.createElement("h4");
-      cardWay.innerText = "How to :"
-      cardBtm.appendChild(cardWay);
-      // creation of instructions frame
-      const cardInstruct = document.createElement("p");
-      cardInstruct.innerText = `${this.state.resultDrinks.strInstructions}`;
-      cardInstruct.classList.add("cardInstruct");
-      cardBtm.appendChild(cardInstruct);
-      //append the whole card to the container
-      document.getElementById('cardCont').appendChild(cardBody);
     }
 
   render(){
@@ -171,17 +63,16 @@ class CardSystem extends React.Component {
       <section id="searchEngine">
         <div className="inputLine">
           <div>
-            <input id="searchName" type="text" placeholder="Enter a name:" onChange={this.searchName}></input>
+            <input id="searchName" type="text" value={this.state.research} placeholder="Enter a name:" onChange={this.searchName}></input>
             <button onClick={this.fetchDrinksByName}>Search</button>
           </div>
-          <button onClick={this.createCardbyName}>Create</button>
         </div>
         <div className="inputLine2">
-          <button onClick={this.fetchDrinksRandom}>Search Random</button>
-          <button onClick={this.createCardRandom}>Create Random</button>
+          <button onClick={this.fetchDrinksRandom}>Random</button>
         </div>
       </section>
       <section id="cardCont">
+        {this.state.resultDrinks.map((drink, index) => <Card drink={drink} key={index} drinkIngredients={this.getAllIngre(drink)} />)}
       </section>
       </div>
     )
