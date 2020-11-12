e4ae561a662462f462d7523cbead441ea866e789
Maxence-P <maxencepointeau@gmail.com>
2020-11-12 15:31:40 +0100

    finished

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,7 +11,7 @@ class ContactUs extends Component {
     };
 
 
-    handleChange = (event) => {  console.log(event.target)  
+    handleChange = (event) => {  
         this.setState({[event.target.name] : event.target.value});
     };
      
